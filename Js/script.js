8c477cc0ec62e6ceec33a16cefe35fa5a6236764
Monikernique <122692155+Monikernique@users.noreply.github.com>
2023-04-03 20:36:31 -0400

    bootstrap files for future editing
    included more files

--- Js/script.js
+++ Js/script.js
@@ -1,14 +1,32 @@
+/*var images = ['bg_01.jpg',
+          'bg_02.jpg', 
+          'bg_03.jpg', 
+          'bg_04.jpg', 
+          'bg_05.jpg', 
+          'bg_06.jpg', 
+          'bg_07.jpg', 
+          'bg_08.jpg', 
+          'bg_09.jpg', 
+          'bg_10.jpg',
+          'bg_11.jpg',
+          "bg_12.jpg",
+          "bg_13.jpg",
+          "bg_14.jpg",];
+$('#bg').css({'background-image' : 'url(images/' + images[Math.floor(Math.random() * images.length)] + ')'});*/
 
+
+
+/*Background*/ 
 $(window).scroll(function() {
     // Get scroll position
     var s = $(window).scrollTop(),
     // scroll value and opacity
-    opacityVal = (s / 600.0);
+    opacityVal = (s / 800.0);
     // opacity value 0% to 100%
     $('.blurred-img').css('opacity', opacityVal);
 });
 
-
+/*form*/
 function openForm() {
   document.getElementById("myForm").style.display = "block";
   
@@ -18,3 +36,13 @@ function closeForm() {
   document.getElementById("myForm").style.display = "none";
 
 }
+
+
+
+
+/*
+
+  window.close()
+ click anywhere outside of form
+
+  */
